32d6a888c55274ef7295420e731e3f3a9b8f24d1
Krzysztof Gryc <hello@krzysztofg.pl>
2020-09-24 12:48:58 +0200

    connect to DB

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    ()=>console.log('Connected to DB')
+);
 
 //Import Routes
 const authRoute = require('./routes/auth');
