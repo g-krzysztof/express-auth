9de475c676f7110dc63adf6aaab082d5e7371d66
Krzysztof Gryc <hello@krzysztofg.pl>
2020-10-28 22:36:59 +0100

    update error response - register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,7 +8,7 @@ router.post('/register', async (req, res) => {
 
     //Validate data before make a user
     const {error} = registerValidation(req.body)
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send({error: error.details[0].message});
 
     //Checking if the user already exist
     const emailExist = await User.findOne({
