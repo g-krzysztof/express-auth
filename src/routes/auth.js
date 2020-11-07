4daf23562ac9e36b4f04d6f2d77c0b67d8ead839
Krzysztof Gryc <hello@krzysztofg.pl>
2020-11-07 21:46:22 +0100

    fix error messages

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -14,7 +14,7 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({
         email: req.body.email
     });
-    if (emailExist) return res.status(400).send('Email already exists:');
+    if (emailExist) return res.status(400).send({ error: 'Email already exists:' });
 
     //Hash passwords
     const salt = await bcrypt.genSalt(10);
@@ -39,21 +39,21 @@ router.post('/login', async (req, res) => {
 
     //Validate data
     const {error} = loginValidation(req.body)
-    if (error) return res.status(400).send({error: error.details[0].message});
+    if (error) return res.status(400).send({ error: error.details[0].message });
 
     //Checking if the user already exist
     const user = await User.findOne({
         email: req.body.email
     });
-    if (!user) return res.status(400).send('Email or password is wrong');
+    if (!user) return res.status(400).send({ error: 'Email or password is wrong' });
 
     //Checking if password is correct
     const validPass = await bcrypt.compare(req.body.password, user.password);
-    if(!validPass) return res.status(400).send('Invalid password');
+    if(!validPass) return res.status(400).send({ error: 'Invalid password' });
 
     //Create and assign a token
     const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-    res.header('auth-token', token).send({token: token, user: user._id, name: user.name});
+    res.header('auth-token', token).send({ token: token, user: user._id, name: user.name });
 
 });
 
